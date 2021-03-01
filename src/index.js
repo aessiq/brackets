39219b46affa60cdbe8945552ac6e11442e9f880
aessiq <noriod@yandex.ru>
2021-03-01 13:36:27 +0300

    feat: solve part of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let strArr = str.split('');
+  for (let i = 0; i < strArr.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (strArr[i] == bracketsConfig[j][0]) {
+        if (strArr[i+1] == bracketsConfig[j][1]) {
+          strArr.splice(i, 2);
+          i--;
+        }
+      }
+    }
+  }
+  if (strArr.length == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}  
+
+// check('()', [['(', ')']]) // -> true
+// check('((()))()', [['(', ')']]) // -> true
+// check('())(', [['(', ')']]) // -> false
+// check('([{}])', [['(', ')'], ['[', ']'], ['{', '}']]) // -> true
+// check('[(])', [['(', ')'], ['[', ']']]) // -> false
+// check('[]()', [['(', ')'], ['[', ']']]) // -> true
+// check('[]()(', [['(', ')'], ['[', ']']]) // -> false
